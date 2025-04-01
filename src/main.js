326124f43119420025d36da853f35f88c0bc53bf
Brodie Heginbotham <bheginbotham@mac.lan>
2025-04-01 12:12:45 -0400

    local packaged code instead of cdn call

--- src/main.js
+++ src/main.js
@@ -1,8 +1,14 @@
-import { toPng} from 'html-to-image';
+import { defineCustomElements } from "@bitjson/qr-code";
+import { toPng } from 'html-to-image';
 import download from 'downloadjs';
 
-var downloadFile = () => {toPng(document.getElementById('qr1'))
-  .then((dataUrl) => download(dataUrl, 'qr-image.png'));}
+// Call this to define the custom element for <qr-code>
+defineCustomElements(window);
+
+var downloadFile = () => {
+    toPng(document.getElementById('qr1'))
+    .then((dataUrl) => download(dataUrl, 'qr-image.png'));
+}
 
 const generateQR = () => {
     const url = document.getElementById("urlInput").value.trim();
@@ -10,20 +16,23 @@ const generateQR = () => {
     const qrCode = document.getElementById("qr1");
     qrCode.setAttribute("contents", url);
     const container = document.getElementById("url-container");
-    container.innerHTML=""
+    container.innerHTML = "";
     const h1 = document.createElement("h1");
-    const text = document.createTextNode(`Download QR Code for ${url}`)
-    h1.appendChild(text)
-    container.appendChild(h1)
+    const text = document.createTextNode(`Download QR Code for ${url}`);
+    h1.appendChild(text);
+    container.appendChild(h1);
 }
 
 document.getElementById("generateBtn").addEventListener("click", function() {
-    generateQR()
-})
+    generateQR();
+});
+
 document.getElementById("urlInput").addEventListener("keypress", function(e) {
-    if(e.key == "Enter") {generateQR()}
-})
+    if (e.key == "Enter") {
+        generateQR();
+    }
+});
 
 document.getElementById("downloadBtn").addEventListener("click", function() {
     downloadFile();
-})
+});
